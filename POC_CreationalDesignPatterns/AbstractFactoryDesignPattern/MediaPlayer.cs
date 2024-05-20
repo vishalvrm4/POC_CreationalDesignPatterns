2ecea154be99e83fae8fc70ca630aa4a11f30f5b
vishalvrm4 <vishalvrm4@users.noreply.github.com>
2024-05-20 15:41:25 +0530

    abstract

---
+++ POC_CreationalDesignPatterns/AbstractFactoryDesignPattern/MediaPlayer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDesignPattern
+{
+    public interface IPlayButton
+    {
+        void Render();
+    }
+
+    public interface IStopButton
+    {
+        void Render();
+    }
+
+    public class WindowPlayButton : IPlayButton
+    {
+        public void Render() => Console.WriteLine("Play music in Windows ...... ");
+    }
+
+    public class WindowStopButton : IStopButton
+    {
+        public void Render() => Console.WriteLine("Stop music in Windows ...... ");
+    }
+
+    public class MacPlayButton : IPlayButton
+    {
+        public void Render() => Console.WriteLine("Play music in Mac ...... ");
+    }
+
+    public class MacStopButton : IStopButton
+    {
+        public void Render() => Console.WriteLine("Stop music in Mac ...... ");
+    }
+
+
+    /***************************************** Abstract Factory *****************/
+    public interface IMediaControlFactory
+    {
+        IPlayButton PlayButton();
+        IStopButton StopButton();
+    }
+
+    public class WindowMediaControl : IMediaControlFactory
+    {
+        public IPlayButton PlayButton()
+        {
+            return new WindowPlayButton();
+        }
+
+        public IStopButton StopButton()
+        {
+            return new WindowStopButton();
+        }
+    }
+
+    public class MacMediaControl : IMediaControlFactory
+    {
+        public IPlayButton PlayButton() 
+        {
+            return new MacPlayButton();
+        }
+
+        public IStopButton StopButton()
+        {
+            return new MacStopButton();
+        }
+    }
+
+    public class MediaPlayer
+    {
+        private readonly IPlayButton playButton;
+        private readonly IStopButton stopButton;
+
+        public MediaPlayer(IMediaControlFactory media)
+        {
+            playButton = media.PlayButton();
+            stopButton = media.StopButton();
+        }
+
+        public void RenderControls()
+        {
+            playButton.Render();
+            stopButton.Render();
+        }
+    }
+}
---
+++ POC_CreationalDesignPatterns/AbstractFactoryDesignPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractFactoryDesignPattern;
+
+Console.WriteLine("Hello, World!");
+
+IMediaControlFactory factory = null;
+MediaPlayer player = null;
+
+factory = new WindowMediaControl();
+player = new MediaPlayer(factory);
+player.RenderControls();
+
+factory = new MacMediaControl();
+player = new MediaPlayer(factory);
+player.RenderControls();
