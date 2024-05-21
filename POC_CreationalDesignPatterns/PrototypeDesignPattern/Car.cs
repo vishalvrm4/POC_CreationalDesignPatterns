ae6a615f2a20a549b3cb3bd8e239d488f774275e
vishalvrm4 <vishalvrm4@users.noreply.github.com>
2024-05-21 11:52:30 +0530

    Prototype

---
+++ POC_CreationalDesignPatterns/PrototypeDesignPattern/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesignPattern
+{
+    public interface IPrototype<T>
+    {     
+        T Clone();
+    }
+
+    public class Car : IPrototype<Car>
+    {
+        public string Brand { get; set; }
+        public string Year { get; set; }
+        public string Model { get; set; }
+
+        public Car(string _brand, string _year, string _model)
+        {
+            Brand = _brand;
+            Year = _year;
+            Model = _model;
+        }
+
+        public Car Clone()
+        {
+            return (Car)this.MemberwiseClone();
+        }
+
+        public void GetDetails() => Console.WriteLine($"Brand : {Brand}\tYear : {Year}\tModel : {Model}");
+    }
+}
---
+++ POC_CreationalDesignPatterns/PrototypeDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PrototypeDesignPattern;
+
+Console.WriteLine("Hello, World!");
+
+
+Car carSedan = new Car("Honda", "2022", "Sedan");
+carSedan.GetDetails();
+
+// clone
+Car carHatchback = carSedan.Clone();
+carHatchback.Model = "Hatchback";
+carHatchback.GetDetails();
+
+if (carHatchback == carSedan)
+    Console.WriteLine("Both object same");
